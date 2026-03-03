63a2edb4dfb510723f8a6257bde81ed1cd26bba6
kong-droid <rhdalgid134@gmail.com>
2026-03-03 17:59:37 +0900

    apply config.

--- src/main/kotlin/com/example/ses/config/AwsSesConfig.kt
+++ src/main/kotlin/com/example/ses/config/AwsSesConfig.kt
@@ -1,4 +1,29 @@
 package com.example.ses.config
 
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider
+import software.amazon.awssdk.regions.Region
+import software.amazon.awssdk.services.ses.SesClient
+
+@Configuration
+@ConfigurationProperties(prefix = "cloud.aws")
 class AwsSesConfig {
+    lateinit var region: String
+    lateinit var accessKey: String
+    lateinit var secretKey: String
+
+    @Bean
+    fun sesClient(): SesClient {
+        val builder = SesClient.builder()
+            .region(Region.of(this.region))
+            .credentialsProvider(
+                StaticCredentialsProvider.create(
+                    AwsBasicCredentials.create(this.accessKey, this.secretKey)
+                )
+            )
+        return builder.build()
+    }
 }
--- src/main/kotlin/com/example/ses/config/SwaggerConfig.kt
+++ src/main/kotlin/com/example/ses/config/SwaggerConfig.kt
@@ -1,4 +1,24 @@
 package com.example.ses.config
 
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.info.Info
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@ConfigurationProperties(prefix = "swagger.info")
 class SwaggerConfig {
+    lateinit var title: String
+    lateinit var version: String
+    lateinit var description: String
+
+    @Bean
+    fun openApi(): OpenAPI {
+        val info = Info()
+            .version(this.version)
+            .title(this.title)
+            .description(this.description)
+        return OpenAPI().info(info)
+    }
 }
