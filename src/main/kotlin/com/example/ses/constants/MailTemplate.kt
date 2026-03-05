154aa41ff1a2c526d4979f36ab79fb5d67315b54
kong-droid <rhdalgid134@gmail.com>
2026-03-05 14:39:44 +0900

    add test templates.

--- src/main/kotlin/com/example/ses/constants/MailTemplate.kt
+++ src/main/kotlin/com/example/ses/constants/MailTemplate.kt
@@ -1,7 +1,7 @@
 package com.example.ses.constants
 
-enum class MailTemplate(val subject: String) {
-    WELCOME("환영합니다."),
-    HELLO_WORLD("Hello World!"),
-    BUG("버그 발생!")
+enum class MailTemplate(val subject: String, val templateName: String) {
+    WELCOME("환영합니다.", "welcome.html"),
+    HELLO_WORLD("Hello World!", "hello-world.html"),
+    BUG("버그 발생!", "bug.html")
 }
